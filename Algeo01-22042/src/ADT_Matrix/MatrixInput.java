a6fdb0cc1a13aeef0b1322c17256fe05b9dc3bb1
Amalia Putri <amaliaputriii2104@gmail.com>
2023-09-25 00:09:59 +0700

    feat(ADT_Matrix): debugging

--- Algeo01-22042/src/ADT_Matrix/MatrixInput.java
+++ Algeo01-22042/src/ADT_Matrix/MatrixInput.java
@@ -1,34 +1,36 @@
 package ADT_Matrix;
+
 import java.io.*;
 import java.util.*;
 import menu.*;
 
-public class NatrixInput{
-    public static int choose(){
+public class MatrixInput {
+    public static int choose() {
         Scanner choice;
-        int pilih;
-        boolean prog = true;
-        choice = new(System.in);
+        int pilih = 0;
+        boolean program = true;
+        choice = new Scanner(System.in);
 
-        while(program){
+        while (program) {
             System.out.println("===== PILIHAN READ MATRIX =====");
             System.out.println("Pilihan untuk to read matrix:");
             System.out.println("1. Input manual");
             System.out.println("2. Baca dari file .txt (Make sure file .txt sudah ada di folder yang sesuai.)");
-            System.out.println("Choose how to read matrix (ketik angkanya saja 1 atau 2): ")
+            System.out.println("Choose how to read matrix (ketik angkanya saja 1 atau 2): ");
             pilih = choice.nextInt();
 
-            if(pilih == 1 || pilih == 2){
-                prog = false;
-            }
-            else{
-                System.out.println("Input pilihan salah, silakan input ulang.")
+            if (pilih == 1 || pilih == 2) {
+                program = false;
+            } else {
+                System.out.println("Input pilihan salah, silakan input ulang.");
             }
         }
+
+        choice.close();
         return pilih;
     }
 
-    public static long[][] matrix_file() throws Exception{
+    public static long[][] matrix_file() throws Exception {
         Scanner scan, isiFile, matrixScan;
         String namaFile, pathFile;
         File file;
@@ -38,31 +40,33 @@ public class NatrixInput{
         scan = new Scanner(System.in);
         System.out.println("Nama file lengkap dengan type file (e.g.: case1a.txt): ");
         namaFile = scan.nextLine();
+        scan.close();
 
         // Create + print path file
         pathFile = getPathInput(namaFile);
         System.out.println(pathFile);
 
-        try{
+        try {
             // Read file
             file = new File(pathFile);
             isiFile = new Scanner(file);
-            row = 0; col = 0;
-            while(isiFile.hasNextLine()){
+            row = 0;
+            col = 0;
+            while (isiFile.hasNextLine()) {
                 row = (isiFile.nextLine()).split(" ").length;
-                col ++;
+                col++;
             }
             isiFile.close();
-            
+
             System.out.println("Jumlah baris: " + row);
             System.out.println("Jumlah kolom: " + col);
 
             // Read matrix
             long[][] matrix = new long[row][col];
             matrixScan = new Scanner(file);
-            while(matrixScan.hasNextLine()){
-                for (i = 0; i < row; i++){
-                    for (j = 0; j < col; j++){
+            while (matrixScan.hasNextLine()) {
+                for (i = 0; i < row; i++) {
+                    for (j = 0; j < col; j++) {
                         matrix[i][j] = matrixScan.nextLong();
                     }
                 }
@@ -71,56 +75,64 @@ public class NatrixInput{
             return matrix;
         }
 
-        catch(FileNotFoundException e){
+        catch (FileNotFoundException e) {
             long[][] matrix = new long[0][0];
             return matrix;
         }
     }
 
-    public static String getPathInput(String namaFile){
+    public static String getPathInput(String namaFile) {
         String pathFile;
         String path;
 
         path = System.getProperty("user.dir");
-        if(path.contains("src")){
+        if (path.contains("src")) {
             path = path.replaceAll("src", "");
             pathFile = path + "test\\" + namaFile;
-        }
-        else{
+        } else {
             pathFile = path + "\\test\\" + namaFile;
         }
 
         return pathFile;
     }
 
-    public static long[][] matrix_user(){
+    public static long[][] matrix_user() {
         int m, n, i, j;
         Scanner scan = null;
+
         try {
             // Input m dan m
             scan = new Scanner(System.in);
-            System.out.println("Jumlah aris matrix : ");
+            System.out.println("Jumlah baris matrix : ");
             m = scan.nextInt();
             System.out.println("Jumlah kolom matrix : ");
             n = scan.nextInt();
-  
+
             // Declare matrix
             long[][] matrix = new long[m][n];
-  
+
             // Input elemen matrix
             System.out.println("Input elemen-elemen matrix");
-            for (i = 0; i < m; i++){
-                for (j = 0; j < n; j++){
+            for (i = 0; i < m; i++) {
+                for (j = 0; j < n; j++) {
                     matrix[i][j] = scan.nextLong();
                 }
             }
             return matrix;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (scan != null) {
+                scan.close();
+            }
         }
-        catch (Exception e) {}
     }
 
     public int getRowEff(long[][] matrix){
         if(choose() == 1){
+            m = new ();
             return matrix_user.(m);
         }
         else{
@@ -137,7 +149,7 @@ public class NatrixInput{
         }
     }
 
-    public long getElmt(long[][] matrix, int i, int j){
+    public long getElmt(long[][] matrix, int i, int j) {
         return matrix_file[i][j];
     }
 }
