df60c9b1f664d3ec46801f7db5cfef4fd5386d2a
wigaandini <erdiantiwiga925@gmail.com>
2023-09-25 07:10:56 +0700

    feat: rev output, add zero row, add row multiply, add col multiply

--- src/ADT_Matrix/MatrixOutput.java
+++ src/ADT_Matrix/MatrixOutput.java
@@ -1,14 +1,16 @@
 package ADT_Matrix;
+
+import java.io.*;
+import java.util.*;
+
 public class MatrixOutput{
-    public static String printMatrix (double[][] matrix){
+    public static void printMatrix (double[][] matrix){
         int i,j ;
-        String str = "";
         for (i = 0; i < matrix.length; i++) {
             for (j = 0; j < matrix[0].length; j++) {
-                str = str + matrix[i][j] + " "; // Print each element
+                System.out.print(matrix[i][j] + " "); // Print each element
             }
-            str = str + "\n";
+            System.out.println();
         }
-        return str;
     }
 }
