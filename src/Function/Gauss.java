724760597152b4048fcc5afb6fb1186d2de069c6
wigaandini <erdiantiwiga925@gmail.com>
2023-09-29 15:50:16 +0700

    feat: benerin syntax gauss

--- src/Function/Gauss.java
+++ src/Function/Gauss.java
@@ -11,28 +11,6 @@ public class Gauss {
         // OBE matriks gauss
         int row = MatrixOP.getRowEff(m);
         int col = MatrixOP.getColEff(m);
-        // OBE matriks gauss
-        int row = MatrixOP.getRowEff(m);
-        int col = MatrixOP.getColEff(m);
-
-        int i = 0;
-        int j = 0;
-
-        while (i < row && j < col) {
-            // mencari pivot
-            int k = i;
-            while (k < row && m[k][j] == 0) {
-                k++;
-            }
-
-            if (k < row) {
-                // swap baris
-                MatrixOP.swapRow(m, i, k);
-
-                // membuat pivot menjadi 1
-                double pivot = m[i][j];
-                for (int l = j; l < col; l++) {
-                    m[i][l] /= pivot;
         int i = 0;
         int j = 0;
 
@@ -69,24 +47,6 @@ public class Gauss {
         }
 
         return m;
-
-                // membuat kolom menjadi 0
-                for (int l = 0; l < row; l++) {
-                    if (l != i) {
-                        double factor = m[l][j];
-                        for (int n = j; n < col; n++) {
-                            m[l][n] -= factor * m[i][n];
-                        }
-                    }
-                }
-                i++;
-
-            }
-            j++;
-        }
-
-        return m;
-
     }
 
     public static void matriksGauss(double[][] m) {
@@ -94,23 +54,6 @@ public class Gauss {
         MatrixOutput.printMatrix(forwardOBE(m));
     }
 
-    public static void solGauss(double[][] m) {
-        double[][] matrix = forwardOBE(m);
-        int rowM = MatrixOP.getRowEff(matrix);
-        int colM = MatrixOP.getColEff(matrix);
-
-        System.out.println("Solusi SPL:");
-        if (MatrixOP.solTidakAda(matrix)) {
-            System.out.println("Tidak ada");
-        } else if (MatrixOP.solBanyak(matrix)) {
-            System.out.println("Banyak");
-        } else {
-            // mencari solusi SPL tunggal
-        }
-    }
-
-    // public static double[][] mxSolGauss(double[][] m) {
-    // double[][] matrix = forwardOBE(m);
     public static void solGauss(double[][] m) {
         double[][] matrix = forwardOBE(m);
         int rowM = MatrixOP.getRowEff(matrix);
