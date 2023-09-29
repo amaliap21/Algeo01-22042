d85883b0cb2a73a32f1302e554c6117549b286d2
Amalia Putri <amaliaputriii2104@gmail.com>
2023-09-29 10:39:51 +0700

    .

--- src/Function/Inverse.java
+++ src/Function/Inverse.java
@@ -8,17 +8,17 @@ public class Inverse {
     // metode cofactor
     public static double[][] inverseMatriks(double[][] m) {
         if (MatrixOP.determinant(m) == 0) {
-            System.out.println("Determinan matriks 0 / Matriks bukan merupakan matriks persegi, tidak bisa dibalikkan");
+            System.out.println("Determinan matriks 0, tidak bisa dibalikkan");
             return null;
         }
 
-        return (MatrixOP.multiplyByConst(Cofactor.adj(m), (1 / Cofactor.determinanCof(m))));
+        return (MatrixOP.multiplyByConst(Cofactor.adj(m), (1 / MatrixOP.determinant(m))));
     }
 
     // print matriks balikan (cofactor)
     public static void matriksInverse(double[][] m) {
         if (MatrixOP.determinant(m) == 0) {
-            System.out.println("Determinan matriks 0 / Matriks bukan merupakan matriks persegi, tidak bisa dibalikkan");
+            System.out.println("Determinan matriks 0, tidak bisa dibalikkan");
         } else {
             System.out.println("Matriks balikan (menggunakan matriks kofaktor):");
             MatrixOutput.printMatrix(inverseMatriks(m));
@@ -26,51 +26,6 @@ public class Inverse {
     }
 
     // metode Gauss-Jordan ([A|I] = [I|A^-1])
-    public static double[][] gaussJordanBalikan(double[][] m) {
-        // MatriksX itu tuh matriks yang bakal jadi hasil Matriks dari metode
-        // Gauss-Jordan
-        double[][] matrix = MatrixOP.copyMatrix(m);
-
-        // Index baris dan kolom yang efektif dari matriks m atau matrix (sama aja)
-        int rowM = MatrixOP.getRowEff(matrix);
-        int colM = MatrixOP.getColEff(matrix);
-
-        // Proses metode Gauss-Jordan
-        for (int i = 0; i < rowM; i++) {
-            // Nyari indeks dengan elemen terbesar pada suatu kolom
-            // Tujuan: buat swapping baris
-            int max = i;
-            for (int j = i + 1; j < rowM; j++) {
-                if (Math.abs(matrix[j][i]) > Math.abs(matrix[max][i])) {
-                    max = j;
-                }
-            }
-
-            // Swapping baris
-            double[] temp = matrix[i];
-            matrix[i] = matrix[max];
-            matrix[max] = temp;
-
-            // Buat elemennya jadi 1
-            double divider = matrix[i][i];
-            for (int j = i; j < colM; j++) {
-                matrix[i][j] /= divider;
-            }
-
-            // Ngurangin baris lain dengan baris yang sudah dikurangi
-            for (int j = 0; j < rowM; j++) {
-                if (j != i) {
-                    double multiplier = matrix[j][i];
-                    for (int k = i; k < colM; k++) {
-                        matrix[j][k] -= multiplier * matrix[i][k];
-                    }
-                }
-            }
-        }
-
-        return matrix;
-    }
-
     public static double[][] balikanGJ(double[][] m) {
         int rowM = MatrixOP.getRowEff(m);
 
@@ -98,15 +53,24 @@ public class Inverse {
             }
         }
 
-        mGabung = gaussJordanBalikan(mGabung);
+        mGabung = GaussJordan.gaussJordan(mGabung);
 
+        // // Ngambil matriks balikan dari matriks mGabung
+        // double[][] mBalikan = new double[rowM][rowM];
+        // for (int i = 0; i < rowM; i++) {
+        // for (int j = rowM; j < rowM * 2; j++) {
+        // mBalikan[i][j - rowM] = mGabung[i][j];
+        // }
+        // }
+
+        // return mBalikan;
         return mGabung;
     }
 
     // print matriks balikan (Gauss-Jordan)
     public static void matriksInverseGJ(double[][] m) {
         if (MatrixOP.determinant(m) == 0) {
-            System.out.println("Determinan matriks / Matriks bukan merupakan matriks persegi, tidak bisa dibalikkan");
+            System.out.println("Determinan matriks 0, tidak bisa dibalikkan");
         } else {
         }
         System.out.println("Matriks Balikan Gauss-Jordan:");
