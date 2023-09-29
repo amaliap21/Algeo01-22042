b8509ff4197c0e2d51a8ace6280fd6605299a7dd
Amalia Putri <amaliaputriii2104@gmail.com>
2023-09-29 10:40:27 +0700

    .

--- src/Function/Cofactor.java
+++ src/Function/Cofactor.java
@@ -5,7 +5,6 @@ import java.util.*;
 import ADT_Matrix.*;
 
 public class Cofactor {
-
     public static double determinanCof(double[][] m) {
         double det;
         int tanda, j;
@@ -28,16 +27,11 @@ public class Cofactor {
     }
 
     public static double[][] getCofactor(double[][] m, int row, int col) {
-        double[][] sub, newM;
+        double[][] newM;
         int i, j;
         newM = MatrixOP.copyMatrix(m);
 
-        if (MatrixOP.isSquare(newM)) {
-            sub = new double[MatrixOP.getRowEff(newM) - 1][MatrixOP.getColEff(newM) - 1];
-        } else {
-            sub = new double[MatrixOP.getRowEff(newM) - 1][MatrixOP.getRowEff(newM) - 1];
-        }
-
+        double[][] sub = new double[MatrixOP.getRowEff(newM) - 1][MatrixOP.getColEff(newM) - 1];
         for (i = 0; i < MatrixOP.getRowEff(sub); i++) {
             for (j = 0; j < MatrixOP.getColEff(sub); j++) {
                 if (i < row && j < col) {
@@ -65,7 +59,7 @@ public class Cofactor {
         int i, j;
         double detCof;
 
-        if (MatrixOP.isSquare(m)) {
+        // if (MatrixOP.isSquare(m)) {
             cof = new double[MatrixOP.getRowEff(m)][MatrixOP.getColEff(m)];
             for (i = 0; i < MatrixOP.getRowEff(m); i++) {
                 for (j = 0; j < MatrixOP.getColEff(m); j++) {
@@ -85,55 +79,70 @@ public class Cofactor {
                     MatrixOP.setElmt(cof, i, j, detCof);
                 }
             }
-        } else {
-            cof = new double[MatrixOP.getRowEff(m)][MatrixOP.getRowEff(m)];
-            for (i = 0; i < MatrixOP.getRowEff(m); i++) {
-                for (j = 0; j < MatrixOP.getRowEff(m); j++) {
-                    detCof = determinanCof(getCofactor(m, i, j));
-                    if (i % 2 == 0) {
-                        if (j % 2 != 0) {
-                            detCof *= (-1);
-                        }
-                    } else {
-                        if (j % 2 == 0) {
-                            detCof *= (-1);
-                        }
-                    }
-                    if (detCof == -0.0) {
-                        detCof = 0.0;
-                    }
-                    MatrixOP.setElmt(cof, i, j, detCof);
-                }
-            }
-        }
+            return cof;
+        } 
+        // else {
+        //     cof = new double[MatrixOP.getRowEff(m)][MatrixOP.getRowEff(m)];
+        //     for (i = 0; i < MatrixOP.getRowEff(m); i++) {
+        //         for (j = 0; j < MatrixOP.getRowEff(m); j++) {
+        //             detCof = MatrixOP.determinant(getCofactor(m, i, j));
+        //             if (i % 2 == 0) {
+        //                 if (j % 2 != 0) {
+        //                     detCof *= (-1);
+        //                 }
+        //             } else {
+        //                 if (j % 2 == 0) {
+        //                     detCof *= (-1);
+        //                 }
+        //             }
+        //             if (detCof == -0.0) {
+        //                 detCof = 0.0;
+        //             }
+        //             MatrixOP.setElmt(cof, i, j, detCof);
+        //         }
+        //     }
+        // }
 
-        return cof;
-    }
+    // }
 
     public static double[][] adj(double[][] m) {
         return MatrixOP.transpose(createMatrixCofactor(m));
     }
 
-    public static double detByCofactor(double[][] cof, double[][] m, int n) {
-        System.out.println("index merupakan baris/kolom?");
-        System.out.println("Jika baris ketik '1'");
-        System.out.println("Jika kolom ketik '2'");
-        Scanner jenis = new Scanner(System.in);
-        int choose = jenis.nextInt();
-        double det = 0;
-        int i, j;
-
-        if (choose == 1) {
-            for (j = 0; j < MatrixOP.getRowEff(m); j++) {
-                det = det + cof[n][j] * m[n][j];
-            }
-        } else if (choose == 2) {
-            for (i = 0; i < MatrixOP.getRowEff(m); i++) {
-                det = det + cof[i][n] * m[i][n];
-            }
-        } else {
-            System.out.println("Input salah, silakan ulangi.");
+    public static void detByCofactor(double[][] cof, double[][] m, int n) {
+        if (MatrixOP.isSquare(m) == false){
+            System.out.println("Matriks ini tidak memiliki determinan karena bukan berukuran nxn");
         }
+        else{
+            System.out.println("Index merupakan baris/kolom?");
+            System.out.println("Jika baris ketik '1'");
+            System.out.println("Jika kolom ketik '2'");
+            Scanner jenis = new Scanner(System.in);
+            int choose = jenis.nextInt();
+            double det = 0;
+            int i, j;
+            if(choose == 1){
+                for(j = 0; j < MatrixOP.getRowEff(m); j++){
+                    det = det + cof[n][j]*m[n][j];
+                }
+            } 
+            else if (choose == 2) {
+                for (i = 0; i < MatrixOP.getRowEff(m); i++) {
+                    det = det + cof[i][n] * m[i][n];
+                }
+            } 
+            else {
+                System.out.println("Input salah, silakan ulangi.");
+            }
+            System.out.println("Determinan matriks ini adalah " + det);
+        }
+    }
+
+    public static double valDetCofactor(double[][] cof, double[][] m, int n) {
+        double det = 0;
+        for(int j = 0; j < MatrixOP.getRowEff(m); j++){
+            det = det + cof[n][j]*m[n][j];
+        } 
         return det;
     }
 }
